8e47dc97b798a0e30836e9e0b4fde47d260fbf0b
JacquelynJohnson <jacquelyn0890@gmail.com>
2019-10-16 00:09:00 -0500

    added methods for button clicks. create comboBox. needs password textfield and complete submitMouseClicked

--- SIMS/src/sims/newUsr.java
+++ SIMS/src/sims/newUsr.java
@@ -6,7 +6,9 @@
 package sims;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.io.IOException;
 
 /**
  *
@@ -16,27 +18,50 @@ public class newUsr extends JFrame{
     public int bp;      //bp is the user defined admin or student profile to create
     JFrame ui = new JFrame();
     JPanel disp = new JPanel();
-    JComboBox profile = new JComboBox();
+    private final String[] profSelect = {"Student","Administrator"};
+    JComboBox profile = new JComboBox(profSelect);
     JTextField showID = new JTextField(10);
     JPasswordField adminCheck = new JPasswordField(4);
-    JButton quit = new JButton("Cancel");
+    JButton quit, submit;
+    JPanel grid = new JPanel();
+    int newID;
+    String newPass; //needs to be setup
     
     /**
      * no argument constructor for the class
      */
     public newUsr(){
         makeHalfScreen(ui);
+        quit = new JButton("Cancel");
+        submit = new JButton("Submit");
+        showID.setEditable(false);
+        grid.setLayout(new GridLayout(0,2));
+        disp.add(profile, BorderLayout.WEST);
+        disp.add(showID, BorderLayout.CENTER);
+        grid.add(quit); grid.add(submit);
+        
+        ui.add(disp, BorderLayout.WEST);
+        ui.add(grid, BorderLayout.SOUTH);
+        
+        //ui.add(submit, BorderLayout.SOUTH);
+        
         ui.setTitle("Create New User Profile");
         ui.setLocationRelativeTo(null);
         ui.setVisible(true);
-        showID.setEditable(false);
         
-        disp.add(profile, BorderLayout.WEST);
-        disp.add(showID, BorderLayout.CENTER);
+        quit.addMouseListener(new java.awt.event.MouseAdapter(){
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent ae){
+                quitMouseClicked(ae);
+            }
+        });
         
-        ui.add(disp, BorderLayout.WEST);
-        ui.add(quit, BorderLayout.SOUTH);
-        
+        submit.addMouseListener(new java.awt.event.MouseAdapter(){
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent ae){
+                submitMouseClicked(ae);
+            }
+        });
     }
     
     public newUsr(int a){
@@ -46,7 +71,7 @@ public class newUsr extends JFrame{
     
     /**
      * sets the frame size to that of half the size of the screen of the user
-     * @param aF frame to be sized to half the screensize
+     * @param aF frame to be sized to half the screenSize
      */
     private void makeHalfScreen(JFrame aF){
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -54,8 +79,29 @@ public class newUsr extends JFrame{
     }
     
     /**
-     * private method to handle quit button click. will dispose the frame and freeze 
-     * anything associated with it.
+     * method for cancel button click
      */
+    private void quitMouseClicked(java.awt.event.MouseEvent ae){
+        new SIMS();
+    }
     
+    /**
+     * method for submit mouse click
+     */
+    private void submitMouseClicked(java.awt.event.MouseEvent ae){
+        //capture all the information from user input and feed it to the database here
+        
+        //after it's sent to the database, refactor and return to the login option
+    }
+    
+    /**
+     * method for generating a new ID. will pull last ID entry from the database,
+     * add 1 to the last digit, and return it as the newID for the newUsr
+     * @return the new ID number for the user and display it in the showID JTextField
+     */
+    public int newIDGen(){
+        //pull the last ID entry from the database
+        newID = 10000001 +1;
+        return newID;
+    }
 }
