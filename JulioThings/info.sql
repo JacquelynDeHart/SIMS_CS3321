581c147919bd89208c3db83435434023592e6fca
Julio Quintero <sabrejq77@gmail.com>
2019-11-18 12:38:27 -0600

    Add files via upload

---
+++ JulioThings/info.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS `info`;
+CREATE DATABASE `info`;
+USE `info`;
+
+CREATE TABLE `student_info` (
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `birth_date` date DEFAULT NULL,
+  `password` int (11) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `student_info` VALUES (1,'Sean','Quintero', '1991-11-17', 111);
+INSERT INTO `student_info` VALUES (2,'Ines','Brushfield','1986-04-13', 222);
+INSERT INTO `student_info` VALUES (3,'Freddi','Boagey','1985-02-07', 333);
+INSERT INTO `student_info` VALUES (4,'Levy','Mynett','1969-10-13', 444);
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(50) NULL,
+  `student_id`int(11) NOT NULL,
+  `exam_one` int(11) NULL,
+  `final_exam` int(11) NULL,
+  `gpa` decimal(4,2) NULL,
+  PRIMARY KEY (`course_id`), 
+  FOREIGN KEY (`student_id`) REFERENCES `student_info` (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `courses` VALUES (1,'Operating Systems',1, NULL,NULL,NULL);
+
